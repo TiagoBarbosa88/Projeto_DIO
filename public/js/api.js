c867aa0fc96f523262259a98db2d2c442ef026fe
TiagoBarbosa88 <sombr43@gmail.com>
2022-12-18 10:40:50 -0300

    api

---
+++ public/js/api.js
@@ -0,0 +1,6 @@
+let api_key = "e7a4b88b1fb4277a33d7f148bdce1799"
+
+let image_url = "https://image.tmdb.org/t/p/w500/"
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discoveri/movie?"
