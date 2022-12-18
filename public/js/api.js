5cccbba678d47871609234de1407dd9faca62f05
TiagoBarbosa88 <sombr43@gmail.com>
2022-12-18 11:59:42 -0300

    finished

--- public/js/api.js
+++ public/js/api.js
@@ -1,6 +1,7 @@
 let api_key = "e7a4b88b1fb4277a33d7f148bdce1799"
 
-let image_url = "https://image.tmdb.org/t/p/w500/"
+let img_url = "https://image.tmdb.org/t/p/w500"
+
 let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
 
-let movie_genres_http = "https://api.themoviedb.org/3/discoveri/movie?"
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
